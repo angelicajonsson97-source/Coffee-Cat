295af1e4e4efbe4265ad20578f88753f922e8e72
Angelica Jonsson Landström <angelicajonsson24@gmail.com>
2026-01-20 11:23:28 +0100

    uppdatering

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,17 +1,15 @@
 import "./Components/Assets/Styles/App.css";
 import Header from "./Components/Header/Header.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Cat from "./Components/Pages/cat";
 import Home from "./Components/Pages/Home";
 import Coffee from "./Components/Pages/Coffee";
 import Footer from "./Components/Footer/Footer.jsx";
+import NotFound from "./Components/Pages/NotFound.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
       <div className="app">
-
-        {/* Din nav-stil ligger kvar här */}
         <nav className="nav">
           <Header />
         </nav>
@@ -21,11 +19,12 @@ export default function App() {
             <Route path="/Coffee-Cat" element={<Home />} />
             <Route path="/Cat" element={<Cat />} />
             <Route path="/Coffee" element={<Coffee />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
 
         <Footer />
       </div>
-    </BrowserRouter>
+
   );
 }
---
+++ react-app/src/Components/Pages/NotFound.jsx
@@ -0,0 +1,9 @@
+export
+default function NotFound() {
+  return (
+    <main>
+      <h1>Sidan kunde inte hittas</h1>
+      <p>Tyvärr, sidan du söker finns inte. Kontrollera webbadressen eller gå tillbaka till startsidan.</p>
+    </main>
+  );
+} 
